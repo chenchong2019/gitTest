b6b94cde5ff9f18cc4df40a5ac42578eb30c9a08
chenchong <chenchong1004@163.com>
2019-04-01 13:27:20 +0800

    test merge tool

--- src/main/java/gitTest/Happy.java
+++ src/main/java/gitTest/Happy.java
@@ -2,7 +2,6 @@ package gitTest;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("left commit........");
-		System.out.println("right test......");
+		System.out.println("right ........");
 	}
 }
