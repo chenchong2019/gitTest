1aed1ad6cfd1cf4382304857e51db12cd7f216ca
chenchong <chenchong1004@163.com>
2019-04-01 11:34:27 +0800

    test conflict

--- src/main/java/gitTest/Apple.java
+++ src/main/java/gitTest/Apple.java
@@ -1,9 +1,11 @@
 package gitTest;
 
 public class Apple {
-
 	public static void main(String[] args) {
-		System.out.println("merge test00001.......");
+		System.out.println("merge test.......");
+		System.out.println("left test.......");
+		System.out.println("left test001.......");
+		System.out.println("merge test002.......");
 	}
 
 }
