8bbf7d77c9f9523f9ceb7b8bcb360e996fab79d9
chenchong <chenchong1004@163.com>
2019-04-01 11:35:29 +0800

    test conflict

--- src/main/java/gitTest/Apple.java
+++ src/main/java/gitTest/Apple.java
@@ -4,6 +4,8 @@ public class Apple {
 
 	public static void main(String[] args) {
 		System.out.println("merge test002.......");
+		System.out.println("right test001.......");
+		System.out.println("right test002.......");
 	}
 
 }
